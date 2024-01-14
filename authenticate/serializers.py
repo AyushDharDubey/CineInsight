09ede20f957438e4cc3c16fbb682984a8f25a904
Ayush Dhar Dubey <mailaddubey@gmail.com>
2024-01-14 23:52:45 +0530

    .

--- authenticate/serializers.py
+++ authenticate/serializers.py
@@ -1,6 +1,8 @@
 from .models import User
 from rest_framework import serializers
 from .utils import send_account_activation
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,6 +12,10 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User(**validated_data)
         user.set_password(validated_data['password'])
+        try:
+            validate_password(password=validated_data['password'], user=user)
+        except ValidationError as e:
+            raise serializers.ValidationError({'password': e.messages})
         user.save()
         send_account_activation(user)
         return user
