c4bc53ddd5dae413c4ac28f7a58bbae001a4d1f3
Ayush Dhar Dubey <mailaddubey@gmail.com>
2024-01-18 12:00:09 +0530

    .

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 BACKEND_BASE_HOST = os.environ.get('BACKEND_BASE_HOST')
 BACKEND_BASE_URL = os.environ.get('BACKEND_BASE_URL')
